46498c42868a766ec5185ad4403341b796f281de
unknown <vivek.kumar450028@gmail.com>
2025-02-09 19:35:17 +0530

    new elements

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import * as ReactDOM from "react-dom";
+import './App.css'
+import Header from "./components/Header";
+
+function App() {
+  
+
+  return (
+    <>
+    <Header name="Vivek" year={new Date().getFullYear()}/> 
+    <main>creates the most adavanced  softwares !!!</main>
+    </>
+  )
+}
+
+export default App
